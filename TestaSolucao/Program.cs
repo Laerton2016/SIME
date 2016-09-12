e4a6c7d495c89076969d0e6611ab3ad2eecce2ad
Laerton Marques de Figueiredo <laerton281003@hotmail.com>
2016-09-12 17:05:48 -0300

    Ajustes minimos

--- TestaSolucao/Program.cs
+++ TestaSolucao/Program.cs
@@ -18,16 +18,16 @@ namespace TestaSolucao
         {
             try
             {
-                SIME.Class.DAO.DAOProduto dao = new DAOProduto();
-                NetProduto produto = dao.Buscar(21);
-                Console.WriteLine(produto);
+               // SIME.Class.DAO.DAOProduto dao = new DAOProduto();
+                //NetProduto produto = dao.Buscar(21);
+                //Console.WriteLine(produto);
                 //Console.Read();
-                //new SIME.Estorna(39646);
+                new SIME.Estorna(39741);
                 //SIME.Estorna estorno = new SIME.Estorna();
                 //estorno.ajustaprec(1.13, 48);
                 //Ajuste ajuste = new Ajuste();
                 //ajuste.AjustarAll(3);
-                //Console.WriteLine("Cancelamento de venda realizado.");
+                Console.WriteLine("Cancelamento de venda realizado.");
             }
             catch (Exception e)
             {
